f2568e767d6a07933c8cfaefb2223ee6a7bb37a7
naufalweise <naufalwwidyatama@gmail.com>
2025-03-18 10:30:42 +0700

    fix get bootstrap server output

--- operator/scripts/get-bootstrap-server.py
+++ operator/scripts/get-bootstrap-server.py
@@ -1,5 +1,8 @@
 import subprocess
-print("ip")
-subprocess.run("minikube ip")
-print("port")
-subprocess.run('kubectl get svc my-cluster-kafka-external-bootstrap -n kafka -o jsonpath="{.spec.ports[0].nodePort}"')
+result = subprocess.run("minikube ip", stdout=subprocess.PIPE, text=True)
+ip = result.stdout.strip()
+
+result = subprocess.run('kubectl get svc my-cluster-kafka-external-bootstrap -n kafka -o jsonpath="{.spec.ports[0].nodePort}"', stdout=subprocess.PIPE, text=True)
+port = result.stdout.strip()
+
+print(f"{ip}:{port}")
