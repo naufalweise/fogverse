4945ec34991c75272f8c062d442f74be34302f10
Naufal Weise <naufal.weise@ui.ac.id>
2025-05-11 10:45:53 +0700

    Update install sh

--- operator/scripts/install.sh
+++ operator/scripts/install.sh
@@ -1,4 +1,3 @@
-minikube start --driver=hyperv
 kubectl create namespace kafka
 kubectl create -f "https://strimzi.io/install/latest?namespace=kafka" -n kafka
 kubectl apply -f resources/kubernetes-deployments/metrics/kafka-monitoring-config.yaml -n kafka # must be executed to avoid error
